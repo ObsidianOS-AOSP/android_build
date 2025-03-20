70e92d668f6f12e80fc60747a4cb7dc62a857f81
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-03-21 07:02:41 +0800

    Update launcher alias
    Change-Id: I85606d6fcd796f485475a868471992328e35ba3f

--- envsetup.sh
+++ envsetup.sh
@@ -1355,7 +1355,7 @@ function axionSync() {
 function biApp() {
     local package="$1"
     if [[ "$package" == "L3" ]]; then
-        package="Launcher3QuickStep"
+        package="TrebuchetQuickStep"
     elif [[ "$package" == "SG" ]]; then
         package="SettingsGoogle"
     fi
